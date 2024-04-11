25f092c43351362e8ff10e6b67f82e1392171462
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2024-04-11 16:08:10 +0300

    create user.py

---
+++ app/user.py
@@ -0,0 +1,19 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import create_app
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(120), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    profile_picture = db.Column(db.String(128))
+    bio = db.Column(db.Text)
+    gaming_preferences = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
