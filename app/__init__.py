f007b6da2ef4fad06796e7ef733982c5768d3444
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2024-04-11 16:08:42 +0300

    imported SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 from app import pages
 
+
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates/pages')
+    app = Flask(__name__, template_folder='templates')
     app.secret_key = "@secretkey#"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    class Base(DeclarativeBase):
+        pass
+    db = SQLAlchemy(model_class=Base)
 
     app.register_blueprint(pages.bp)
     return app
