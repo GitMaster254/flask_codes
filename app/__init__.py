1ca1a3e8e37b4ae609837092af57c517d65135d5
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2024-04-11 18:34:41 +0300

    Refactor __init__.py to initialize and create the database

--- app/__init__.py
+++ app/__init__.py
@@ -17,8 +17,9 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    
     db.init_app(app)
     with app.app_context():
         db.create_all()
+    app.run(debug=True)
     
